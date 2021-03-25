9a1b68ae8cb29d002d3f6bdf2ab9187fc1319822
marqueswsm <wagner_marques@live.com>
2021-03-25 19:32:23 -0300

    solve some build errors

--- src/infrastructure/adapter/schema/user.ts
+++ src/infrastructure/adapter/schema/user.ts
@@ -1,13 +1,8 @@
 import * as mongoose from 'mongoose';
-import * as paginate from 'mongoose-paginate-v2';
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
   _id: String,
   name: String,
   username: String,
   priority: Number,
 });
-
-userSchema.plugin(paginate);
-
-export { userSchema };
